66c7c2495eae51f485d374bbfe496515b3f893e1
Rheno Febrian <rfebrianc@gmail.com>
2025-03-11 21:15:57 +0700

    edit axios

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,11 +3,9 @@ import axios, { AxiosInstance } from "axios";
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}?q=${import.meta.env.VITE_QUERY}`,
   headers: {
-    "Upgrade-Insecure-Requests": "1",
     "Content-Type": "application/json",
   },
   params: {
-    q: import.meta.env.VITE_QUERY,
     apiKey: import.meta.env.VITE_API_KEY,
   },
 });
